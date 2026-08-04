88e58e404408e5c30a55e502524c8f16d2698deb
denis-moscow-crypto <denis-moscow@bk.ru>
2026-08-04 13:54:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //  ВАЖНО: ВСТАВЬ СВОИ КЛЮЧИ SUPABASE!
 // =====================================================
 const SUPABASE_URL = 'https://oreexiwvjhwssznwxndn.supabase.co';
-const SUPABASE_KEY = 'h4fqK0XozeXtNU6JBaBC7w_Psj6ACXV';
+const SUPABASE_KEY = 'sb_publishable_PMaFGTxugOMCVAZAfbRV_A_2I0i9tK';
 // =====================================================
 
 const tg = window.Telegram.WebApp;
